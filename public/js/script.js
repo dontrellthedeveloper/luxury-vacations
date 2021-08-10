036711af6bb12882d995ea8078dae543f3a89afd
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-08-10 05:10:40 -0400

    added team section html, css, and javascript

--- public/js/script.js
+++ public/js/script.js
@@ -6,3 +6,19 @@ $(window).on('load', function() {
     $('#preloader').delay(350).fadeOut('slow');
 });
 
+/* ================================
+|   |   |   Team
+================================ */
+$(function () {
+    $("#team-members").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+    });
+});
+
