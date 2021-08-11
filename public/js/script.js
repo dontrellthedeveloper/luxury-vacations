65af07c2c81ebe0383be50c48227988dfd667540
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-08-10 22:20:43 -0400

    added navigation

--- public/js/script.js
+++ public/js/script.js
@@ -175,3 +175,68 @@ $(window).on('load', function () {
     });
 
 });
+
+
+
+/* ================================
+|   |   |   Navigation
+================================ */
+
+/* Show & Hide White Navigation */
+$(function () {
+
+    // show/hide nav on page load
+    showHideNav();
+
+    $(window).scroll(function () {
+
+        // show/hide nav on window's scroll
+        showHideNav();
+
+    });
+
+    function showHideNav() {
+
+        if( $(window).scrollTop() > 50 ) {
+
+            // Show white nav
+            $("nav").addClass("white-nav-top");
+
+            // Show dark logo
+            $(".navbar-brand img").attr("src", "img/logo/logo-dark.png");
+
+            // Show back to top
+            $("#back-to-top").fadeIn();
+
+        } else {
+
+            // Hide white nav
+            $("nav").removeClass("white-nav-top");
+
+            // Show logo
+            $(".navbar-brand img").attr("src", "img/logo/logo.png");
+
+            // Show back to top
+            $("#back-to-top").fadeOut();
+        }
+    }
+
+});
+
+// Smooth Scrolling
+$(function () {
+
+    $("a.smooth-scroll").click(function(event) {
+
+        event.preventDefault();
+
+        // get selection id like #about, #services, #work, #team and etc.
+        var section_id = $(this).attr("href");
+
+        $("html, body").animate({
+            scrollTop: $(section_id).offset().top - 64
+        }, 1250, "easeInOutExpo");
+
+    });
+
+});
