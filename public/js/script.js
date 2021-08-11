2592195f4c674c6f7a74312f44ba613692a74948
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-08-10 20:54:33 -0400

    added google maps

--- public/js/script.js
+++ public/js/script.js
@@ -140,3 +140,38 @@ $(function() {
         navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
     });
 });
+
+
+/* ================================
+|   |   |   Google Map
+================================ */
+$(window).on('load', function () {
+
+    // Map Variables
+    var addressString = '230 Broadway, NY, New York 10007, USA';
+    var myLatlng = {lat: 40.712685, lng: -74.005920};
+
+    //1. Render Map
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 11,
+        center: myLatlng
+    });
+
+    // 2. Add Marker
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: "Click To See Address"
+    });
+
+    // 3. Add Info Window
+    var infowindow = new google.maps.InfoWindow({
+        content: addressString
+    });
+
+    // Show info window when user clicks marker
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
+    });
+
+});
