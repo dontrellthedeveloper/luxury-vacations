cdf9a0371d2c5d426086b550c8e611eb06cf336e
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-08-11 00:59:55 -0400

    updated logo and navigation

--- public/js/script.js
+++ public/js/script.js
@@ -230,7 +230,7 @@ $(function () {
             $("nav").addClass("white-nav-top");
 
             // Show dark logo
-            $(".navbar-brand img").attr("src", "img/logo/logo-dark.png");
+            $(".navbar-brand img").attr("src", "img/logo/vacation-logo-light.png");
 
             // Show back to top
             $("#back-to-top").fadeIn();
@@ -241,7 +241,7 @@ $(function () {
             $("nav").removeClass("white-nav-top");
 
             // Show logo
-            $(".navbar-brand img").attr("src", "img/logo/logo.png");
+            $(".navbar-brand img").attr("src", "img/logo/vacation-logo-light.png");
 
             // Show back to top
             $("#back-to-top").fadeOut();
