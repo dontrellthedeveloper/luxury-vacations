3d3b437bf37022220840a813ad19ee9b713865f4
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-08-10 22:24:23 -0400

    added media queries for large, medium & small devices

--- public/js/script.js
+++ public/js/script.js
@@ -18,7 +18,15 @@ $(function () {
         autoplayHoverPause: true,
         nav: true,
         dots: false,
-        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+        navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            }
+        }
     });
 });
 
