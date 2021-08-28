6631bd9171cb9e3f0271702624f9bed9a4a4f41c
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-08-28 19:19:40 -0400

    added user routes

--- app.js
+++ app.js
@@ -120,6 +120,51 @@ const deleteVacation = (req, res) => {
 };
 
 
+
+
+const getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined!'
+    })
+};
+
+const createUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined!'
+    })
+};
+
+const getUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined!'
+    })
+};
+
+const updateUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined!'
+    })
+};
+
+const deleteUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined!'
+    })
+};
+
+
+
+
+
+
+
+
+
 // 3. Routes
 
 // app.get('/api/v1/tours', getAllVacations);
@@ -139,6 +184,18 @@ app
     .patch(updateVacation)
     .delete(deleteVacation);
 
+
+
+app
+    .route('/api/v1/users')
+    .get(getAllUsers)
+    .post(createUser);
+
+app
+    .route('/api/v1/users/:id')
+    .get(getUser)
+    .patch(updateUser)
+    .delete(deleteUser);
 
 
 // 4. Start Server
