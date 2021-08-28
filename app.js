8f1931848e42d4b8e3819db01307922ac50fbb4c
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-08-28 02:08:46 -0400

    added the ability to handle get requests

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Hello from the server side!', app: 'Vacations'});
-});
+// app.get('/', (req, res) => {
+//   res.status(200).json({message: 'Hello from the server side!', app: 'Natours'});
+// });
+//
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...')
+// });
 
-app.post('/', (req, res) => {
-    res.send('You can post to this endpoint...')
+const vacations = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/vacation-simple-2.json`)
+);
+
+app.get('/api/v1/vacations', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: vacations.length,
+        data: {
+            vacations
+        }
+    })
 });
 
 const port = 3000;
