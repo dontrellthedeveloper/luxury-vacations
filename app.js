0ff183d9fa1224ce6963c46e9ad7bc451b58e0cd
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-08-28 02:22:31 -0400

    added the ability to handle post requests

--- app.js
+++ app.js
@@ -3,13 +3,7 @@ const express = require('express');
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//   res.status(200).json({message: 'Hello from the server side!', app: 'Natours'});
-// });
-//
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint...')
-// });
+
 
 const vacations = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/vacation-simple-2.json`)
@@ -24,6 +18,26 @@ app.get('/api/v1/vacations', (req, res) => {
         }
     })
 });
+
+
+
+app.post('/api/v1/vacations', (req, res) => {
+
+    const newId = vacations[vacations.length - 1].id + 1;
+    const newVacation = Object.assign({id: newId}, req.body);
+
+    vacations.push(newVacation);
+
+    fs.writeFile(`${__dirname}/dev-data/data/vacation-simple-2.json`, JSON.stringify(vacations), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                vacation: newVacation
+            }
+        })
+    });
+});
+
 
 const port = 3000;
 app.listen(port, () => {
