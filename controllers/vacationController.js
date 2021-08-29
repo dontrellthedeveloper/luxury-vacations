505b5d86871a1c170d53d4437b4c32c72d904b83
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-08-29 01:40:22 -0400

    refactored API Queries

--- controllers/vacationController.js
+++ controllers/vacationController.js
@@ -1,6 +1,6 @@
 const Vacation = require('./../models/vacationModel');
 
-
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.aliasTopVacations = (req, res, next) => {
     req.query.limit = '5';
@@ -13,51 +13,13 @@ exports.aliasTopVacations = (req, res, next) => {
 
 exports.getAllVacations = async  (req, res) => {
     try {
-        // BUILD QUERY
-        // 1A) Filtering
-        const queryObj = {...req.query};
-        const excludedFields = ['page', 'sort', 'limit', 'fields'];
-        excludedFields.forEach(el => delete queryObj[el]);
-
-        // 1B) Advanced Filtering
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-        console.log(JSON.parse(queryStr));
-
-        let query = Vacation.find(JSON.parse(queryStr));
-
-        // 2) Sorting
-        if(req.query.sort) {
-            const sortBy = req.query.sort.split(',').join(' ');
-            console.log(sortBy);
-            query = query.sort(sortBy);
-        } else {
-            query = query.sort('-createdAt');
-        }
-
-        // 3) Field limiting
-        if (req.query.fields) {
-            const fields = req.query.fields.split(',').join(' ');
-            query = query.select(fields);
-        } else {
-            query = query.select('-_v');
-        }
-
-        // 4) Pagination
-        const page = req.query.page * 1 || 1;
-        const limit = req.query.limit * 1 || 100;
-        const skip = (page - 1) * limit;
-
-        query = query.skip(skip).limit(limit);
-
-        if (req.query.page) {
-            const numVacations = await Vacation.countDocuments();
-            if (skip >= numVacations) throw new Error('This page does not exist');
-        }
-
-
         // EXECUTE QUERY
-        const vacations = await query;
+        const features = new APIFeatures(Vacation.find(), req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate();
+        const vacations = await features.query;
 
         // SEND RESPONSE
         res.status(200).json({
