4c91f386fe421b18c4db113214ed33896426fdcb
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-08-29 00:19:18 -0400

    added the ability to import data into mongodb using js file

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Vacation = require('./../../models/vacationModel');
+
+
+dotenv.config({path: './config.env'});
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true
+    // useCreateIndex: true,
+    // useFindAndModify: false
+}).then(() => console.log('DB connection successful'));
+
+const vacations = JSON.parse(fs.readFileSync(`${__dirname}/vacation-simple-2.json`,'utf-8'));
+
+
+
+//IMPORT DATA INTO DB
+const importData = async () => {
+    try {
+        await Vacation.create(vacations);
+        console.log('Data successfully loaded');
+        process.exit();
+    } catch (e) {
+        console.log(e)
+    }
+};
+
+// DELETE DATA FROM DB
+const deleteData = async () => {
+    try {
+        await Vacation.deleteMany();
+        console.log('Data deleted successfully!');
+    } catch (e) {
+        console.log(e)
+    }
+    process.exit();
+};
+
+if(process.argv[2] === '--import') {
+    importData();
+}
+else if (process.argv[2] === '--delete') {
+    deleteData();
+}
