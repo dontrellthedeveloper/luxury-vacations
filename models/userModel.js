691889b6606e5eaabf9ba3ddddac39fcc174556b
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-08-29 23:44:29 -0400

    added check for password validation and created a password encryption using bcryptjs

--- models/userModel.js
+++ models/userModel.js
@@ -26,9 +26,29 @@ const userSchema = new mongoose.Schema({
     passwordConfirm: {
         type: String,
         required: [true, 'Please confirm your password'],
-        minlength: 8
+        validate: {
+            // This only works on create and save!!!
+            validator: function(el) {
+                return el === this.password;
+            },
+            message: 'Passwords are not the same!'
+        }
     }
 });
+
+
+userSchema.pre('save', async function(next) {
+    // Only run this function if password was actually modified
+    if(!this.isModified('password')) return next();
+
+    // Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    // Delete the passwordConfirm field
+    this.passwordConfirm = undefined;
+    next();
+});
+
 
 const User = mongoose.model('User', userSchema);
 
