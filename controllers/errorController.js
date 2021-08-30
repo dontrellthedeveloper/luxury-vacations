4a6d0be580fa5259c0db82540c9aed2de303e839
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-08-29 21:44:06 -0400

    added ability to catch uncaught exceptions

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -37,7 +37,7 @@ const sendErrorProd = (err, res) => {
         //Programming or other unknown error: don't leak error details
 
         // 1) Log error
-        console.error('ERROR', err);
+        console.error('ERROR 💥', err);
 
         // 2) Send generic message
         res.status(500).json({
