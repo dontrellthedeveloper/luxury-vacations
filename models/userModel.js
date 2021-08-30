0207b69f5a69faca3071fb773eb0bde3c86bbd6a
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-08-29 23:58:35 -0400

    finished protected routes algorithm for user login. User must have the correct jwt in their header to be authenticated to use protected routes.

--- models/userModel.js
+++ models/userModel.js
@@ -34,7 +34,8 @@ const userSchema = new mongoose.Schema({
             },
             message: 'Passwords are not the same!'
         }
-    }
+    },
+    passwordChangedAt: Date
 });
 
 
@@ -57,6 +58,15 @@ userSchema.methods.correctPassword = async function(
     return await bcrypt.compare(candidatePassword, userPassword);
 };
 
+userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+    if(this.passwordChangedAt) {
+        const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
+        console.log(changedTimestamp, JWTTimestamp);
+        return JWTTimestamp < changedTimestamp;
+    }
+
+    return false;
+};
 
 const User = mongoose.model('User', userSchema);
 
