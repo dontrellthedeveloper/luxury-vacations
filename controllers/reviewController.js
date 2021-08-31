a05431fe27cf80c62094ba0bd0ef01007b52b457
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-08-30 23:00:54 -0400

    added a nested GET Endpoint

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,7 +2,10 @@ const Review = require('./../models/reviewModel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.getAllReviews = catchAsync(async (req,res,next) => {
-    const reviews = await Review.find();
+    let filter = {};
+    if (req.params.vacationId) filter = {tour: req.params.vacationId};
+
+    const reviews = await Review.find(filter);
 
     res.status(200).json({
         status: 'success',
