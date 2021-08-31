d35c5367ec3ee6f3770c3a7f843e1845fbe73cef
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-08-30 22:59:26 -0400

    using nested routes with express

--- routes/vacationRoutes.js
+++ routes/vacationRoutes.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const vacationController = require('./../controllers/vacationController');
 const authController = require('./../controllers/authController');
-const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
 
 // router.param('id', vacationController.checkID);
+
+// router
+//   .route('/:tourId/reviews')
+//   .post(
+//     authController.protect, authController
+//     .restrictTo('user'), reviewController.createReview
+//   );
+router.use('/:vacationId/reviews', reviewRouter);
 
 router
     .route('/top-5-cheap')
@@ -29,10 +37,10 @@ router
         authController.restrictTo('admin', 'lead-guide'),
         vacationController.deleteVacation);
 
-router
-    .route('/:vacationId/reviews')
-    .post(authController.protect, authController
-        .restrictTo('user'), reviewController.createReview
-    );
+// router
+//     .route('/:vacationId/reviews')
+//     .post(authController.protect, authController
+//         .restrictTo('user'), reviewController.createReview
+//     );
 
 module.exports = router;
